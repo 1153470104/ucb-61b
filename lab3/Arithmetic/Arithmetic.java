5ea9dead1a46c8411ea97aa682ac368fc67dd5d3
t34shen <t34_shen@163.com>
2020-02-12 22:12:30 +0800

    start 61b lab3

---
+++ lab3/Arithmetic/Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+    /** Computes product of two ints */
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    /** Computes sum of two ints (incorrectly) */
+
+    /** what...
+     *  the resolution is just to change the operator "*" into "+" */
+    public static int sum(int a, int b) {
+        return a + b;
+    }   
+}
