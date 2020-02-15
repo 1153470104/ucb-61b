66013af1d1bdbaf5c5ca38526dc13645aa4b3424
t34shen <t34_shen@163.com>
2020-02-15 21:35:46 +0800

    start NBody.java

---
+++ proj0/NBody.java
@@ -0,0 +1,10 @@
+
+/** NBody is going to stimulate a universe specified in a txt file */
+public class NBody {
+	/** read the radious of the universe */
+	public double readRadious(String file) {
+		double radious;
+		
+		return radious;
+	}
+}
