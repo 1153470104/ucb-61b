de24f11ccaa84b04c140240c7714e030d2923e35
t34shen <t34_shen@163.com>
2020-02-15 22:15:00 +0800

    start first two methods

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,10 +1,32 @@
 
 /** NBody is going to stimulate a universe specified in a txt file */
 public class NBody {
+	
 	/** read the radious of the universe */
-	public double readRadious(String file) {
-		double radious;
+	public static double readRadius(String file) {
+		/** instantiate */
+		In in = new In(file);
 		
-		return radious;
+		in.readInt();
+		double radius = in.readDouble();
+		
+		return radius;
+	}
+
+	/** then read the body of the file */
+	public static Body[] readBodies(String file) {
+		/** instantiate & skip the first two number */
+		In in = new In(file);
+		in.readInt();
+		in.readDouble();
+
+		/** give some midvalue to manipulate the file content */
+		double xP, yP, xV, yV, mass;
+		String img;
+
+		/** the main method loop */
+		while(!in.isEmpty()) {
+			
+		}
 	}
 }
