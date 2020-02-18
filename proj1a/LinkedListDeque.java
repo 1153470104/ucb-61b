d39bde0b6bdbd590309b918ce13358a206df7d8f
t34shen <t34_shen@163.com>
2020-02-18 22:02:04 +0800

    2.18 little change

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+public class LinkedListDeque<T> {
+    /** create an empty linked list deque */
+    public LinkedListDeque() {
 
-public class LinkedListDeque {
+    }
+
+    /** create a deep copy of 'other' */
+    public LinkedListDeque(LinkedListDeque other) {
+
+    }
+
+    /**
+     * the next eight method is the operations of deque
+     * 1-4 manipulating item: add / remove, first / last
+     * 5-8 isempty / size / get / printDeque
+     *
+     * the first 4 methods must take "constant time"
+     * i.e. execution tiem should not depend on the size of the deque
+     */
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    /** take constant time, too */
+    public int size() {
+
+    }
+
+
+    /** get must use iteration, not recursion */
+    public T get(int index) {
+
+    }
+
+    /** same as get, but uses recursion */
+    public  T getRecursive(int index) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void printDeque() {
+
+    }
+
 }
