3e3fe8130dccbda76f6857c7bcef502b6eaed7da
t34shen <t34_shen@163.com>
2020-02-19 21:00:30 +0800

    start ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
 
-public class ArrayDeque {
+public class ArrayDeque<T> {
+
+    /** create an empty linked list deque */
+    public ArrayDeque() {
+
+    }
+
+    /** create a deep copy of 'other' */
+    public ArrayDeque(ArrayDeque other) {
+
+    }
+
+    /**
+     * the amount of memory the program uses
+     * must be proportional to the number of items
+     * the usage factor always be at least 25%
+     */
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    /** take constant time, too */
+    public int size() {
+
+    }
+
+
+    /** get must use iteration, not recursion */
+    public T get(int index) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void printDeque() {
+
+    }
 }
